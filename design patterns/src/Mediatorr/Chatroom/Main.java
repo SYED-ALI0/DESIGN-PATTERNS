76357395835ca96bf7f52d28f16134a5c7e06b4f
SYED-ALI0 <syedrehman13101@gmail.com>
2023-10-20 21:28:56 +0500

    mediator

---
+++ design patterns/src/Mediatorr/Chatroom/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GOF.Mediator.Chatroom;
+
+/**
+ *
+ * @author fa20-bse-025
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        University comsatsUniversity = new University("COMSATS University");
+        University nustUniversity = new University("NUST");
+        Teacher teachermukhtiar = new Teacher("Sir Mukhtiar Zamin ");
+        User hamza = new User("Hamza");
+        User atif = new User("Atif");
+   
+        ChatRoom.sendMessageToTarget(hamza, comsatsUniversity, "Hello, COMSATS University!");
+        ChatRoom.sendMessageToAll(hamza, List.of(comsatsUniversity, nustUniversity, teachermukhtiar, atif), "Hello, Everyone!");
+    }
+
+}
