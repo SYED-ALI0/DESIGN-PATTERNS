dcf407677820a2e16994b147ba4a71a5d34333d8
SYED-ALI0 <syedrehman13101@gmail.com>
2023-12-02 12:36:56 +0500

    command pattern

---
+++ design patterns/src/Command/RemoteControlExample/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Command.RemoteControlExample;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        RemoteControl control = new RemoteControl();
+        Light light = new Light();
+        Command lightsOn = new LightOnCommand(light);
+        Command lightsOff = new LightOffCommand(light);
+
+        // switch on
+        control.setCommand(lightsOn);
+        control.pressButton();
+
+        // switch off
+        control.setCommand(lightsOff);
+        control.pressButton();
+
+    }
+
+}
---
+++ design patterns/src/Command/TextEditorExample/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GOF.Command.TextEditorExample;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
